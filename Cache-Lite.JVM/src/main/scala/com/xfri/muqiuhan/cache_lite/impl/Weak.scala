ac97532628da180a6b55273ac60b080b4196ac7e
Muqiu Han <muqiu-han@outlook.com>
2023-11-13 10:10:33 +0800

    [Cache-Lite.JVM]: [Update]: Support type paramaters for Weak

--- Cache-Lite.JVM/src/main/scala/com/xfri/muqiuhan/cache_lite/impl/Weak.scala
+++ Cache-Lite.JVM/src/main/scala/com/xfri/muqiuhan/cache_lite/impl/Weak.scala
@@ -1,9 +1,33 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023 Muqiu Han
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.xfri.muqiuhan.cache_lite.impl
 
 import java.lang.ref.ReferenceQueue
 import java.lang.ref.WeakReference
 
-import com.xfri.muqiuhan.cache_lite.Cache
+import com.xfri.muqiuhan.cache_lite.GenericCache
 import scala.annotation.tailrec
 
 /** [WeakCache] caches items with a [WeakReference] wrapper.
@@ -13,12 +37,12 @@ import scala.annotation.tailrec
   * collector's ability to determine reachability for you, so you don't have to
   * do it yourself.
   */
-class Weak(delegate: Cache) extends Cache:
+class Weak[K, V](delegate: GenericCache[K, Any]) extends GenericCache[K, V]:
   private case class WeakEntry(
-      key: Any,
-      value: Any,
+      key: K,
+      value: V,
       referenceQueue: ReferenceQueue[Any]
-  ) extends WeakReference[Any](value, referenceQueue)
+  ) extends WeakReference[V](value, referenceQueue)
 
   private val referenceQueue = ReferenceQueue[Any]()
 
@@ -44,17 +68,21 @@ class Weak(delegate: Cache) extends Cache:
   override def size(): Int = delegate.size()
   override def clear(): Unit = delegate.clear()
 
-  override def get(key: Any): Option[Any] =
-    delegate.get(key).flatMap(value => delegate.remove(key).map(_ => value))
+  override def get(key: K): Option[V] =
+    delegate
+      .get(key)
+      .flatMap(value =>
+        delegate.remove(key).map(_ => value.asInstanceOf[WeakEntry].value)
+      )
 
-  override def set(key: Any, value: Any): Unit =
+  override def set(key: K, value: V): Unit =
     removeUnreachableItems()
     delegate.set(key, WeakEntry(key, value, referenceQueue))
 
   /** Since its a weak reference, the returned value is not available So [None]
     * is always returned
     */
-  override def remove(key: Any): Option[Any] =
+  override def remove(key: K): Option[V] =
     delegate.remove(key)
     removeUnreachableItems()
     None
